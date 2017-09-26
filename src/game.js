4e9ae233562bfacf87351d357613e530f412e50c
challeboo <32291313+challeboo@users.noreply.github.com>
2017-09-26 09:45:15 +0200

    Initial commit

---
+++ src/game.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from "./card"
+
+const Game = () => (
+<div className="game" >
+<span><h1> Memory Game </h1></span>
+  <Card src= "/Images/doggo1.jpg"/>
+  <Card src= "/Images/doggo2.jpg"/>
+  <Card src= "/Images/doogo3.jpg"/>
+  <Card src= "/Images/doggo1.jpg"/>
+  <Card src= "/Images/doggo2.jpg"/>
+  <Card src= "/Images/doogo3.jpg"/>
+  </div>
+)
+
+
+export default Game
