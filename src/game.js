5c9472ed5a9f256f85adb9400b3f632187b92184
challeboo <32291313+challeboo@users.noreply.github.com>
2017-09-26 12:03:22 +0200

    Render å sånt

--- src/game.js
+++ src/game.js
@@ -2,17 +2,28 @@ import React from 'react'
 import Card from "./card"
 import Counter from "./counter"
 
-const Game = () => (
-<div className="game" >
-  <Counter />
-  <Card src= "/Images/doggo1.jpg"/>
-  <Card src= "/Images/doggo2.jpg"/>
-  <Card src= "/Images/doogo3.jpg"/>
-  <Card src= "/Images/doggo1.jpg"/>
-  <Card src= "/Images/doggo2.jpg"/>
-  <Card src= "/Images/doogo3.jpg"/>
-  </div>
-)
+class Game extends React.Component {
 
+state = {
+  cards : [
+    {src: "/Images/doggo1.jpg"},
+    {src: "/Images/doggo2.jpg"},
+    {src: "/Images/doogo3.jpg"},
+    {src: "/Images/dog3.jpg"}
+  ]
+}
+
+render () {
+  return (
+  <div className="game" >
+  <Card src={this.state.cards[0].src}/>
+  <Card src={this.state.cards[1].src}/>
+  <Card src={this.state.cards[2].src}/>
+  <Card src={this.state.cards[3].src}/>
+    </div>
+  )
+}
+
+}
 
 export default Game
